7f7d44a841ee3655ab75d8afd68a48c8841edadf
PhoenixTAN <tanziqi1756@outlook.com>
2020-10-28 12:21:18 -0400

    Add annotations and fix format.

--- src/Project.java
+++ src/Project.java
@@ -1,273 +1,207 @@
 import java.io.*;
 
-public class Project
-{
-    public final static void main(String[] argv)
-    {
-        StudentNetworkSimulator simulator;
-        
-        int nsim = -1;
-        double loss = -1;
-        double corrupt = -1;
-        double delay = -1;
-        int trace = -1;
-        int seed = -1;
-	int windowsize = -1;
-	double timeout = -1;
-        String buffer = "";
-        File outputfile = new File("OutputFile");
-        BufferedReader stdIn = new BufferedReader(
-                                   new InputStreamReader(System.in));
-                                   
-        System.out.println("-- * Network Simulator v1.0 * --");
-        
-        while (nsim < 1)
-        {
-            System.out.print("Enter number of messages to simulate (> 0): " +
-                             "[10] ");
-            try
-            {
-                buffer = stdIn.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IOError reading your input!");
-                System.exit(1);
-            }
-            
-            if (buffer.equals(""))
-            {
-                nsim = 10;
-            }
-            else
-            {            
-                try
-                {
-                    nsim = Integer.parseInt(buffer);
-                }
-                catch (NumberFormatException nfe)
-                {
-                    nsim = -1;
-                }
-            }
-        }
-        
-        while (loss < 0)
-        {
-            System.out.print("Enter packet loss probability (0.0 for no " +
-                             "loss): [0.0] ");
-            try
-            {
-                buffer = stdIn.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IOError reading your input!");
-                System.exit(1);
-            }
-            
-            if (buffer.equals(""))
-            {
-                loss = 0;
-            }
-            else
-            {            
-                try
-                {
-                    loss = (Double.valueOf(buffer)).doubleValue();
-                }
-                catch (NumberFormatException nfe)
-                {
-                    loss = -1;
-                }
-            }
-        }            
 
-        while (corrupt < 0)
-        {
-            System.out.print("Enter packet corruption probability (0.0 " +
-                             "for no corruption): [0.0] ");
-            try
-            {
-                buffer = stdIn.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IOError reading your input!");
-                System.exit(1);
-            }
-            
-            if (buffer.equals(""))
-            {
-                corrupt = 0;
-            }
-            else
-            {            
-                try
-                {
-                    corrupt = (Double.valueOf(buffer)).doubleValue();
-                }
-                catch (NumberFormatException nfe)
-                {
-                    corrupt = -1;
-                }
-            }
-        }            
+/**
+ * @author CS-655
+ * @description 
+ * 		This is the Entrance of the program. 
+ * 		1. Enter number of messages.
+ * 		2. Enter packet loss probability.
+ * 		3. Enter packet corruption probability.
+ * 		4. Enter average time between messages from sender's layer 5.
+ * 		5. Enter window size.
+ * 		6. Enter random seed.
+ * 		7. Enter timeout retransmission.
+ * 		Finally, new a StudentNetworkSimulator and run it. 
+ */
+public class Project {
+	public final static void main(String[] argv) {
+		StudentNetworkSimulator simulator;
 
-        while (delay <= 0)
-        {
-            System.out.print("Enter average time between messages from " +
-                             "sender's layer 5 (> 0.0): [1000] ");
-            try
-            {
-                buffer = stdIn.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IOError reading your input!");
-                System.exit(1);
-            }
-            
-            if (buffer.equals(""))
-            {
-                delay = 1000;
-            }
-            else
-            {            
-                try
-                {
-                    delay = (Double.valueOf(buffer)).doubleValue();
-                }
-                catch (NumberFormatException nfe)
-                {
-                    delay = -1;
-                }
-            }
-        }            
+		int numOfMessages = -1;
+		double loss = -1;
+		double corrupt = -1;
+		double delay = -1;
+		int trace = -1;
+		int seed = -1;
+		int windowsize = -1;
+		double timeout = -1;
+		String buffer = "";
+		File outputfile = new File("OutputFile");
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-         while (windowsize < 1)
-        {
-            System.out.print("Enter window size (> 0): [8] ");
-            try
-            {
-                buffer = stdIn.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IOError reading your input!");
-                System.exit(1);
-            }
-            
-            if (buffer.equals(""))
-            {
-                windowsize = 8;
-            }
-            else
-            {            
-                try
-                {
-                    windowsize = Integer.parseInt(buffer);
-                }
-                catch (NumberFormatException nfe)
-                {
-                    windowsize = -1;
-                }
-            }
-        }
+		System.out.println("-- * Network Simulator v1.0 * --");
 
-	while (timeout <= 0)
-        {
-            System.out.print("Enter retransmission timeout (>0.0) [15.0] ");
-            try
-            {
-                buffer = stdIn.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IOError reading your input!");
-                System.exit(1);
-            }
-            
-            if (buffer.equals(""))
-            {
-                timeout = 15.0;
-            }
-            else
-            {            
-                try
-                {
-                    timeout = (Double.valueOf(buffer)).doubleValue();
-                }
-                catch (NumberFormatException nfe)
-                {
-                    timeout = -1;
-                }
-            }
-        }            
+		/* Enter number of messages */
+		while (numOfMessages < 1) {
+			System.out.print("Enter number of messages to simulate (> 0): " + "[10] ");
+			try {
+				buffer = stdIn.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IOError reading your input!");
+				System.exit(1);
+			}
 
-        while (trace < 0)
-        {
-            System.out.print("Enter trace level (>= 0): [0] ");
-            try
-            {
-                buffer = stdIn.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IOError reading your input!");
-                System.exit(1);
-            }
-            
-            if (buffer.equals(""))
-            {
-                trace = 0;
-            }
-            else
-            {            
-                try
-                {
-                    trace = Integer.parseInt(buffer);
-                }
-                catch (NumberFormatException nfe)
-                {
-                    trace = -1;
-                }
-            }
-        }
+			if (buffer.equals("")) {
+				numOfMessages = 10;
+			} else {
+				try {
+					numOfMessages = Integer.parseInt(buffer);
+				} catch (NumberFormatException nfe) {
+					numOfMessages = -1;
+				}
+			}
+		}
 
-        while (seed < 1)
-        {
-            System.out.print("Enter random seed: [0] ");
-            try
-            {
-                buffer = stdIn.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IOError reading your input!");
-                System.exit(1);
-            }
-            
-            if (buffer.equals(""))
-            {
-		seed = 0;
-            }
-            else
-            {            
-                try
-                {
-                    seed = (Integer.valueOf(buffer)).intValue();
-                }
-                catch (NumberFormatException nfe)
-                {
-                    seed = -1;
-                }
-            }
-        }
-         
-        simulator = new StudentNetworkSimulator(nsim, loss, corrupt, delay,
-                                                trace, seed, windowsize, timeout);
-                                                
-        simulator.runSimulator();
-    }
+		/* packet loss probability */
+		while (loss < 0) {
+			System.out.print("Enter packet loss probability (0.0 for no " + "loss): [0.0] ");
+			try {
+				buffer = stdIn.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IOError reading your input!");
+				System.exit(1);
+			}
+
+			if (buffer.equals("")) {
+				loss = 0;
+			} else {
+				try {
+					loss = (Double.valueOf(buffer)).doubleValue();
+				} catch (NumberFormatException nfe) {
+					loss = -1;
+				}
+			}
+		}
+		
+		/* packet corruption probability */
+		while (corrupt < 0) {
+			System.out.print("Enter packet corruption probability (0.0 " + "for no corruption): [0.0] ");
+			try {
+				buffer = stdIn.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IOError reading your input!");
+				System.exit(1);
+			}
+
+			if (buffer.equals("")) {
+				corrupt = 0;
+			} else {
+				try {
+					corrupt = (Double.valueOf(buffer)).doubleValue();
+				} catch (NumberFormatException nfe) {
+					corrupt = -1;
+				}
+			}
+		}
+		
+		/* average time between messages from sender's layer 5 */
+		while (delay <= 0) {
+			System.out.print("Enter average time between messages from " + "sender's layer 5 (> 0.0): [1000] ");
+			try {
+				buffer = stdIn.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IOError reading your input!");
+				System.exit(1);
+			}
+
+			if (buffer.equals("")) {
+				delay = 1000;
+			} else {
+				try {
+					delay = (Double.valueOf(buffer)).doubleValue();
+				} catch (NumberFormatException nfe) {
+					delay = -1;
+				}
+			}
+		}
+		
+		/* window size */
+		while (windowsize < 1) {
+			System.out.print("Enter window size (> 0): [8] ");
+			try {
+				buffer = stdIn.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IOError reading your input!");
+				System.exit(1);
+			}
+
+			if (buffer.equals("")) {
+				windowsize = 8;
+			} else {
+				try {
+					windowsize = Integer.parseInt(buffer);
+				} catch (NumberFormatException nfe) {
+					windowsize = -1;
+				}
+			}
+		}
+		
+		/* retransmission timeout */
+		while (timeout <= 0) {
+			System.out.print("Enter retransmission timeout (>0.0) [15.0] ");
+			try {
+				buffer = stdIn.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IOError reading your input!");
+				System.exit(1);
+			}
+
+			if (buffer.equals("")) {
+				timeout = 15.0;
+			} else {
+				try {
+					timeout = (Double.valueOf(buffer)).doubleValue();
+				} catch (NumberFormatException nfe) {
+					timeout = -1;
+				}
+			}
+		}
+		
+		/* trace level */
+		while (trace < 0) {
+			System.out.print("Enter trace level (>= 0): [0] ");
+			try {
+				buffer = stdIn.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IOError reading your input!");
+				System.exit(1);
+			}
+
+			if (buffer.equals("")) {
+				trace = 0;
+			} else {
+				try {
+					trace = Integer.parseInt(buffer);
+				} catch (NumberFormatException nfe) {
+					trace = -1;
+				}
+			}
+		}
+		
+		/* random seed */
+		while (seed < 1) {
+			System.out.print("Enter random seed: [0] ");
+			try {
+				buffer = stdIn.readLine();
+			} catch (IOException ioe) {
+				System.out.println("IOError reading your input!");
+				System.exit(1);
+			}
+
+			if (buffer.equals("")) {
+				seed = 0;
+			} else {
+				try {
+					seed = (Integer.valueOf(buffer)).intValue();
+				} catch (NumberFormatException nfe) {
+					seed = -1;
+				}
+			}
+		}
+		
+		simulator = new StudentNetworkSimulator(numOfMessages, loss, corrupt, delay, trace, seed, windowsize, timeout);
+
+		simulator.runumOfMessagesulator();
+	}
 }
